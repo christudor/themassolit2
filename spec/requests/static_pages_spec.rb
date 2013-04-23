7d7ece35efc9850edc1d11c074b95073c0272701
Chris Tudor <christudor133@hotmail.com>
2013-04-23 23:05:47 +0100

    Add the first helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "MASSOLIT"}
+
   describe "Home page" do
 
     it "should have the h1 'Massolit'" do
@@ -9,9 +11,14 @@ describe "Static pages" do
       	page.should have_selector('h1', :text => 'Welcome to the MASSOLIT')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "MASSOLIT | Home")
+    	page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have the custom page title" do
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -24,21 +31,35 @@ describe "Static pages" do
 
   	it "should have title 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "MASSOLIT | Help")
+  		page.should have_selector('title', :text => "#{base_title} | Help")
   	end
   end
 
 
   describe "About page" do
 
-  	it "should have the h1 'About the MASSOLIT'" do
+  	it "should have the h1 'About'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => 'About')
   	end
 
   	it "should have the title 'About'" do
   		visit '/static_pages/about'
-  		page.should have_selector('title', :text => "MASSOLIT | About")
+  		page.should have_selector('title', :text => "#{base_title} | About")
   	end
   end
+
+    describe "Contact page" do
+
+  	it "should have the h1 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('h1', :text => 'Contact')
+  	end
+
+  	it "should have the title 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title', :text => "#{base_title} | Contact")
+  	end
+  end
+
 end
