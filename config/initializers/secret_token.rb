cb852dbf4c05f76c408bd55d031bf2432f9982b6
Chris Tudor <christudor133@hotmail.com>
2013-04-23 18:13:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Massolit::Application.config.secret_token = 'eda77558148324a2f63599f5905d64514b93aa5cd593c584afba99bba831c3eaf5745ee1da32ee5a649c263fa0b4164c9d5f3f5fe6428d221608b453f7290fe9'
