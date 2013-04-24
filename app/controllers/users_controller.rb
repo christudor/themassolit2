1093d40bafc52e28dbc1652d7b7780039904ca0d
Chris Tudor <christudor133@hotmail.com>
2013-04-24 12:56:11 +0100

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,12 @@ class UsersController < ApplicationController
 
 	def create
     @user = User.new(params[:user])
-	    if @user.save
-	      flash[:success] = "Welcome to the MASSOLIT"
-	      redirect_to @user
-	    else
-	      render 'new'
-	    end
-  	end
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the MASSOLIT"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
