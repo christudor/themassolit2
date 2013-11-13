683fe2304debc5b8c4cb24fa70b5df6cc0751e71
Chris Tudor <christudor133@hotmail.com>
2013-11-13 14:13:08 +0000

    E-mail problems...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Massolit::Application.configure do
 
    config.action_mailer.smtp_settings = {
     :address              => "smtpout.europe.secureserver.net",
-    :port                 => 465,
+    :port                 => 3535,
     :domain               => "herokuapp.com",
     :user_name            => "chris@massolit.co.uk",
     :password             => ENV["GODADDY_PASSWORD"],
