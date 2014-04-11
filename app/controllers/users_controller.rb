12183b444d99fbfd2cf470538d55cef37dafbb88
Chris Tudor <christudor133@hotmail.com>
2014-04-11 18:03:19 +0100

    Again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,16 +39,12 @@ class UsersController < ApplicationController
         customer = Stripe::Customer.retrieve(user.customer_id)
         unless customer.nil? or customer.respond_to?('deleted')
           if customer.subscription.status == 'active'
-            raise "Customer subscription is active"
             customer.cancel_subscription
           elsif customer.subscription.status == 'trialing'
-            raise "Customer subscription is trialling"
             customer.cancel_subscription
           end
         end
-        raise "Customer was nil or responded to deleted"
       end
-      raise "Customer ID was nil"
       user.destroy
       redirect_to root_path, :notice => "Bye! Thanks for using MASSOLIT! We hope to see you again soon!"
     else
