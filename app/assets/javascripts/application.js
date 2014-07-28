f0e524d633505c03c5ebe1a46f95b589aa01fc82
Chris Tudor <christudor133@hotmail.com>
2014-07-28 19:11:57 +0100

    Touch-screen attempt 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
+//= require bootstrap/bootstrap
 //= require jquery_ujs
 //= require jquery.readyselector
 //= require jquery.externalscript
